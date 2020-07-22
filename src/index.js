04cd1aa8dd5c1cd79fa92e206acbfc443b7ac883
Allan Hyde <allan.hyde@tektools.com.au>
2020-07-22 08:24:06 +0800

    Update index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css';
 //import './style.scss'
 import App from './components/App';
@@ -9,8 +9,8 @@ import App from './components/App';
 ReactDOM.render(
   
   <BrowserRouter>
-        <App />
-   </BrowserRouter>,
+    <App />
+  </BrowserRouter>,
 
   document.getElementById('root')
 );
